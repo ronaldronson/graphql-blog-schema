7db5a06474db261d7001c0e9ae9ba7b1eb8917d2
RRR <romanromenskyi@RRR-MacBookPro.local>
2016-01-04 15:43:20 +0100

    server-side-schema: lesson changes

--- server/schema.js
+++ server/schema.js
@@ -1,3 +1,9 @@
+const mongo = require('promised-mongo');
+// You can use any MONGO_URL here, whether it's locally or on cloud.
+// const db = mongo('mongodb://localhost/mydb');
+const db = mongo('mongodb://foo:bar@ds043997.mongolab.com:43997/af_php-app-website-romenskyi_roman');
+const authorsCollection = db.collection('authors');
+
 import * as _ from 'underscore';
 
 import {
@@ -33,6 +39,23 @@ const Query = new GraphQLObjectType({
   }
 });
 
+// const Mutation = new GraphQLObjectType({
+//   name: 'Mutations',
+//   fields: {
+//     createAuthor: {
+//       type: Author,
+//       args: {
+//         _id: {type: new GraphQLNonNull(GraphQLString)},
+//         name: {type: new GraphQLNonNull(GraphQLString)},
+//         twitterHandle: {type: GraphQLString}
+//       },
+//       resolve: function(rootValue, args) {
+//         throw new Error('Not Implemented')
+//       }
+//     }
+//   }
+// });
+
 const Mutation = new GraphQLObjectType({
   name: 'Mutations',
   fields: {
@@ -44,11 +67,14 @@ const Mutation = new GraphQLObjectType({
         twitterHandle: {type: GraphQLString}
       },
       resolve: function(rootValue, args) {
-        throw new Error('Not Implemented')
+        let author = Object.assign({}, args);
+        return authorsCollection.insert(author)
+          .then(_ => author);
       }
     }
   }
 });
+
 
 const Schema = new GraphQLSchema({
   query: Query,
